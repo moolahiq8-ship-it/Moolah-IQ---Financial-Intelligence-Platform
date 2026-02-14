be43469ce55a3c6565475d77840e6b9d649c272c
moolahiq8-ship-it <moolahiq8@gmail.com>
2026-02-13 17:45:54 -0800

    Add Pinterest tracking tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -48,6 +49,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} ${playfair.variable} flex flex-col min-h-screen`}>
+      {/* Pinterest Tag */}
+            <Script id="pinterest-tag" strategy="afterInteractive">
+              {`
+                !function(e){if(!window.pintrk){window.pintrk = function () {
+                window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+                n=window.pintrk;n.queue=[],n.version="3.0";var
+                t=document.createElement("script");t.async=!0,t.src=e;var
+                r=document.getElementsByTagName("script")[0];
+                r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+                pintrk('load', '2614300006478');
+                pintrk('page');
+              `}
+            </Script>
+            <noscript>
+              <img height="1" width="1" style={{display:'none'}} alt=""
+                src="https://ct.pinterest.com/v3/?event=init&tid=2614300006478&noscript=1" />
+            </noscript>
         <ScrollProgress />
         <Header />
         <main className="flex-1">{children}</main>
